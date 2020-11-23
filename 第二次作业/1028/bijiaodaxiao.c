f014185a0671cb9a5dcbde472054d56ca9fb4f95
wuxukai <1797934615@qq.com>
2020-11-23 09:10:43 +0800

    比大小

---
+++ 第二次作业/1028/bijiaodaxiao.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <windows.h>
+#include <math.h>
+int main()
+{
+	/*int a, b;
+	printf("输入第一个数");
+	scanf_s("%d", &a);
+	printf("输入第二个数");
+	scanf_s("%d", &b);
+	if (a > b)
+		printf("a大于b");
+	else if (a < b)
+
+		printf("a小于b");
+	else
+		printf("a等于b");*/
+
+
+	int a=1;//初始化a
+	int b=2;//初始化b
+	if (a > b)//判断a与b的大小
+		printf("a大于b");//输出
+	else  if(a<b)
+		printf("a小于b");
+   else
+	printf ("a等于b");
+	system("pause");
+	}
+
+
+
+
