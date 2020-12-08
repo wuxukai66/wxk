6b2186625201555192c65c0dff1d63c6177a9654
wuxukai <1797934615@qq.com>
2020-12-08 21:01:30 +0800

    9的个数

--- 分数求和/text2/1-100中9的个数.c
+++ 分数求和/text2/1-100中9的个数.c
@@ -1,23 +1,38 @@
 #include<stdio.h>
 #include<windows.h>
-void geshu()
-{
-	int count=0;
+//void geshu()
+//{
+	/*int count=0;
 	int i;
-	for (i = 1; i < 101; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if (i % 10 == 9)
 		{
 			count++;
 		}
+		if (i / 10 == 9){
+			count++;
+		}
 		
-	}printf("%d", count);
-}
-
-
+	}printf("%d", count);*/
+//}
 int main()
 {
-	geshu();
+	/*geshu();*/
+	int count = 0;
+	int i;
+	for (i = 1; i < 100; i++)
+	{
+		if ((i % 10) == 9)
+		{
+			
+			count++;
+		}
+		if (i / 10 == 9){
+			
+			count++;
+		}
+	}printf("%d", count);
 	system("pause");
 	return 0;
 }
