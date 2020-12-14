ce623c44b81782d26cf2666da7980a9190f80a62
wuxukai <1797934615@qq.com>
2020-12-14 22:22:15 +0800

    练习

---
+++ 练习/src/text1.java
@@ -0,0 +1,16 @@
+//十进制整数1234对应的八进制和十六进制（字母大写），用空格分开，并且要求，在八进制前显示前导0，在十六进制数前显示前导0X。
+public class text1 {
+//    public static void main(String[] args){
+//zhuanhuan(1234);
+//    }
+//    public static void zhuanhuan(int n){
+//        System.out.printf("%#o ",n );
+//        System.out.printf("%#X",n);
+//    }
+//}
+//BoBo写了一个十六进制整数ABCDEF，他问KiKi对应的十进制整数是多少。
+public static void main(String[] args){
+    int n=0XABCDEF;
+    System.out.printf("%d",n);
+}
+}
