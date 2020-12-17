d2b7947e43cf95626d724fefc42733cf110b3718
wuxukai <1797934615@qq.com>
2020-12-17 22:20:03 +0800

    修改

--- 12.17练习/src/Main.java
+++ 12.17练习/src/Main.java
@@ -1,11 +1,14 @@
+//输入3科成绩，然后把三科成绩输出，成绩为整数形式。
+//输入描述:
+//一行，3科成绩，用空格分隔，范围（0~100）
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 Scanner sc=new Scanner(System.in);
 int a=sc.nextInt();
 int b=sc.nextInt();
-int c=sc.nextInt();
-Score(a,b,c);
+int d=sc.nextInt();
+Score(a,b,d);
     }
     public static void Score(int score1,int score2,int score3){
         System.out.print("score1"+"="+score1+",");
--- 12.17练习/src/Main2.java
+++ 12.17练习/src/Main2.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 public class Main2 {
     public static void main(String[] args)throws IOException{
