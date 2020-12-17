42dde546ddcace3086b87cead4264c23a324d363
wuxukai <1797934615@qq.com>
2020-12-17 22:22:03 +0800

    修改2

--- 12.17练习/src/Main2.java
+++ 12.17练习/src/Main2.java
@@ -1,4 +1,6 @@
-
+//依次输入一个学生的学号，以及3科（C语言，数学，英语）成绩，在屏幕上输出该学生的学号，3科成绩。
+//        输入描述:
+//        学号以及3科成绩，学号和成绩之间用英文分号隔开，成绩之间用英文逗号隔开。
 import java.io.*;
 public class Main2 {
     public static void main(String[] args)throws IOException{
